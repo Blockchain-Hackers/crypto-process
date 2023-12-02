475515abfbb1ef51f79281458642aa3275e96572
Favour <gismozfexie@yahoo.com>
2023-12-02 16:09:12 +0100

    feat: add layouts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import { useRoute } from 'vue-router';
+import { LayoutTypes } from '@/types/layouts';
+import Default from '@/layouts/Default.vue';
+import Empty from '@/layouts/Empty.vue';
+import { computed } from 'vue';
+
+const route = useRoute()
+const layout = computed(() => {
+  const layout = route?.meta?.layout
+  if(!layout) return Default // Default layout which require auth
+  if(layout === LayoutTypes.Default) return Default
+  return Empty
+})
 </script>
 
 <template>
-  <RouterView />
+  <component :is="layout" />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/login.vue'
 import Register from '@/views/register.vue'
 import Home from '@/views/index.vue'
+import { LayoutTypes } from '@/types/layouts';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,18 +12,24 @@ const router = createRouter({
       name: 'index',
       component: Home,
       meta: {
-        requiresAuth: true
+        requiresAuth: true,
       }
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        layout: LayoutTypes.Empty
+      }
     },
     {
       path: '/register',
       name: 'register',
-      component: Register
+      component: Register,
+      meta: {
+        layout: LayoutTypes.Empty
+      }
     },
   ]
 })
