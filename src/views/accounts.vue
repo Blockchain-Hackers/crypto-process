0dff6add7e6cdc61f60c5dc9699063fe36643dcd
Davyking <yungdynamic53@gmail.com>
2023-12-05 22:54:19 -0800

    creating modal

--- src/views/accounts.vue
+++ src/views/accounts.vue
@@ -7,24 +7,33 @@
       />
     </div>
 
-    <div>
-      something
+    <div class="tw-p-3">
+      <button
+        class="tw-bg-primary tw-text-white tw-py-4 tw-mt-10 tw-rounded-md tw-font-medium hover:tw-ring-4 tw-ring-primary/30 transition-all tw-duration-300"
+        style="width: 300px"
+        @click="createAccount"
+      >
+        Create Account
+      </button>
     </div>
-    
   </div>
 </template>
 
 <script setup lang="ts">
-import SeedPhraseNotice from '@/components/SeedPhraseNotice.vue';
-import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import SeedPhraseNotice from "@/components/SeedPhraseNotice.vue";
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
-const route = useRoute()
-const router = useRouter()
+const route = useRoute();
+const router = useRouter();
 
-const showNotice = ref(true)
-const handleNoticeVisibility = (show: boolean)  => {
-  console.log('handleNoticeVisibility', show)
-  showNotice.value = show
-}
+const showNotice = ref(true);
+const handleNoticeVisibility = (show: boolean) => {
+  console.log("handleNoticeVisibility", show);
+  showNotice.value = show;
+};
+
+const createAccount = () => {
+  console.log("create account");
+};
 </script>
