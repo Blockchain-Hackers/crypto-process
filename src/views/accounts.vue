3c7475aebc723d55fba500022bbf383ae877d193
Davyking <yungdynamic53@gmail.com>
2023-12-09 03:02:43 -0800

    fixed dialog

--- src/views/accounts.vue
+++ src/views/accounts.vue
@@ -5,11 +5,42 @@
     >
       <h1 class="tw-text-3xl">Create account <strong>to automate</strong></h1>
       <button
+        @click="showDialog = true"
         class="tw-p-2 tw-px-3 tw-bg-primary tw-rounded-lg tw-text-white tw-ring-4 tw-ring-transparent hover:tw-ring-white tw-transition-all tw-duration-300"
       >
         Create account {{}}
       </button>
     </div>
+    <v-dialog
+      v-model="showDialog"
+      :scrim="false"
+      fullscreen
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="!tw-bg-primary">
+        Hello
+
+        <div class="text-center">
+          <v-select
+            v-model="location"
+            :items="locations"
+            label="Location"
+          ></v-select>
+          <v-menu :location="location">
+            <template v-slot:activator="{ props }">
+              <v-btn color="primary" dark v-bind="props"> Dropdown </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </v-card>
+      <!-- <v-card class="!tw-bg-primary"> Hello </v-card> -->
+    </v-dialog>
   </div>
 </template>
 
@@ -32,5 +63,15 @@ const route = useRoute();
 const router = useRouter();
 
 const showNotice = ref(true);
+const showDialog = ref(false);
 const showWorkflowCreation = ref(false);
+const items = ref([
+  { title: "Click Me" },
+  { title: "Click Me" },
+  { title: "Click Me" },
+  { title: "Click Me 2" },
+]);
+
+const locations = ref(["top", "bottom", "start", "end", "center"]);
+const location = ref("end");
 </script>
