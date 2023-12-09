2620d7f59bf8f911d79a2d0fb9a9457ec0a9ef19
Johnpaul <polymenjohn1@gmail.com>
2023-12-09 03:01:24 +0100

    Signup and Login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,68 +1,79 @@
 <template>
   <div class="tw-relative">
     <div
-      class="tw-min-h-[100dvh] tw-flex tw-items-center tw-justify-center
-      tw-max-w-2xl tw-mx-auto tw-p-6 sm:tw-p-10">
+      class="tw-min-h-[100dvh] tw-flex tw-items-center tw-justify-center tw-max-w-2xl tw-mx-auto tw-p-6 sm:tw-p-10"
+    >
       <div class="tw-space-y-10">
         <div class="tw-text-center">
           <h1 class="tw-text-5xl sm:tw-text-6xl tw-font-bold">
             <span class="tw-relative tw-z-10">Automate your work using</span>
             <span
-              class="before:tw-block before:tw-absolute before:-tw-inset-1 before:-tw-skew-y-3
-              before:tw-bg-white tw-border-2 tw-border-black tw-relative tw-inline-block">
+              class="before:tw-block before:tw-absolute before:-tw-inset-1 before:-tw-skew-y-3 before:tw-bg-white tw-border-2 tw-border-black tw-relative tw-inline-block"
+            >
               <span class="tw-relative tw-text-primary">Crypto Process.</span>
             </span>
           </h1>
-        </div>        
+        </div>
 
         <div class="tw-bg-white tw-p-3 sm:tw-p-6 tw-border">
           <div>
             <h2 class="tw-text-3xl tw-font-bold">Welcome back!</h2>
             <form @submit.prevent="handleLogin" class="tw-mt-4 tw-text-xl">
               <label class="tw-relative tw-block">
-                <span class="tw-absolute tw-text-base tw-pl-3 tw-pt-1">Enter your email</span>
+                <span class="tw-absolute tw-text-base tw-pl-3 tw-pt-1"
+                  >Enter your email</span
+                >
                 <input
                   v-model="form.email"
                   type="email"
                   name="email"
                   placeholder="email@example.com"
                   :required="true"
-                  class="tw-w-full tw-bg-gray-100 tw-p-3 tw-pt-7 tw-rounded-md tw-outline-black">
+                  class="tw-w-full tw-bg-gray-100 tw-p-3 tw-pt-7 tw-rounded-md tw-outline-black"
+                />
               </label>
 
               <label class="tw-relative tw-mt-4 tw-flex tw-items-center">
-                <span class="tw-absolute tw-top-0 tw-text-base tw-pl-3 tw-pt-1">Enter your password</span>
+                <span class="tw-absolute tw-top-0 tw-text-base tw-pl-3 tw-pt-1"
+                  >Enter your password</span
+                >
                 <input
                   v-model="form.password"
                   :type="showPassword ? 'text' : 'password'"
                   placeholder="******"
                   :required="true"
-                  class="tw-w-full tw-bg-gray-100 tw-p-3 tw-pt-7 tw-rounded-md tw-outline-black">
-                  <v-icon
-                    @click="()=>showPassword=!showPassword"
-                    :class="[showPassword ? '!tw-text-black' : '!tw-text-gray-300']"
-                    class="!tw-absolute !tw-right-0 !tw-mx-3 tw-bg-gray-100 !tw-rounded-2xl">
-                    {{ showPassword ? 'mdi-eye-outline' : 'mdi-eye-off-outline' }}
-                  </v-icon>
+                  class="tw-w-full tw-bg-gray-100 tw-p-3 tw-pt-7 tw-rounded-md tw-outline-black"
+                />
+                <v-icon
+                  @click="() => (showPassword = !showPassword)"
+                  :class="[
+                    showPassword ? '!tw-text-black' : '!tw-text-gray-300',
+                  ]"
+                  class="!tw-absolute !tw-right-0 !tw-mx-3 tw-bg-gray-100 !tw-rounded-2xl"
+                >
+                  {{ showPassword ? "mdi-eye-outline" : "mdi-eye-off-outline" }}
+                </v-icon>
               </label>
 
               <button
-                class="tw-w-full tw-bg-primary tw-text-white tw-py-4 tw-mt-10
-                tw-rounded-md tw-font-medium hover:tw-ring-4 tw-ring-primary/30
-                transition-all tw-duration-300"
-                :disabled="logingIn">
-                <template v-if="!logingIn">
-                  Login
-                </template>
+                class="tw-w-full tw-bg-primary tw-text-white tw-py-4 tw-mt-10 tw-rounded-md tw-font-medium hover:tw-ring-4 tw-ring-primary/30 transition-all tw-duration-300"
+                :disabled="logingIn"
+              >
+                <template v-if="!logingIn"> Login </template>
                 <v-progress-circular
                   v-else
                   indeterminate
                   color="white"
-                  size="20" width="2">
+                  size="20"
+                  width="2"
+                >
                 </v-progress-circular>
               </button>
               <p class="tw-mt-2 tw-text-sm">
-                Don't have an account? <router-link to="/register" class="tw-text-primary tw-font-bold">Register</router-link>
+                Don't have an account?
+                <router-link to="/register" class="tw-text-primary tw-font-bold"
+                  >Register</router-link
+                >
               </p>
             </form>
           </div>
@@ -70,44 +81,53 @@
       </div>
     </div>
 
-    <v-snackbar
-      v-model="snackbar.show">
+    <v-snackbar v-model="snackbar.show">
       {{ snackbar.text }}
     </v-snackbar>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/auth';
-
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import axios from "axios";
 // useHead({
 //   title: 'Process - Login',
 // })
 
 const form = ref({
-  email: '',
-  password: '',
-})
-const showPassword = ref(false)
+  email: "",
+  password: "",
+});
+const showPassword = ref(false);
 
 const snackbar = ref({
   show: false,
-  text: ''
-})
+  text: "",
+});
 
-const router = useRouter()
-const logingIn = ref(false)
-const authStore = useAuthStore()
+const router = useRouter();
+const logingIn = ref(false);
+const authStore = useAuthStore();
 const handleLogin = () => {
-  logingIn.value = true
-  authStore.login()
-  
+  logingIn.value = true;
 
-  setTimeout(() => {
-    logingIn.value = false
-    router.push('/')
-  }, 2000)
-}
+  axios
+    .post("/v1/auth/login", form.value)
+    .then((res) => {
+      authStore.login(res.data.data.access_token);
+      snackbar.value.show = true;
+      snackbar.value.text = res.data.message;
+      logingIn.value = false;
+      setTimeout(() => {
+        router.push("/");
+      }, 2000);
+    })
+    .catch((err) => {
+      logingIn.value = false;
+      snackbar.value.show = true;
+      snackbar.value.text = err.message;
+    });
+};
 </script>
