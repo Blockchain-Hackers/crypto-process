65080d179c1dad009920a076d871793b219801bf
Johnpaul <polymenjohn1@gmail.com>
2023-12-10 11:25:23 +0100

    set output for triggers and functions in state

--- src/components/WorkflowItem.vue
+++ src/components/WorkflowItem.vue
@@ -1,44 +1,46 @@
 <template>
   <div
-    class="!tw-ring tw-ring-primary/60 !tw-rounded-lg sm:tw-w-[600px]
-    tw-relative tw-bg-white">
-    <div class="tw-flex tw-justify-between tw-items-center tw-p-3">
-      <h3 class="tw-font-mono tw-text-lg tw-truncate">
-        {{
-          isTrigger ?
-            `${ step?.name && activeStepStage==='step_form' ? step?.name : 'Trigger'}` :
-            `${(step?.name && activeStepStage==='step_form') ? step?.name : 'Step'}`
-        }}
-      </h3>
-
-      <v-icon
-        v-if="activeStepStage==='created'"
-        @click="activeStepStage = fantomTabsForStepCreationStage[1]"
-        class="tw-cursor-pointer !tw-text-primary/50 hover:!tw-text-black">
-        mdi-layers-edit
-      </v-icon>
-    </div>
-    <hr>
+    class="!tw-ring tw-ring-primary/60 !tw-rounded-lg sm:tw-w-[600px] tw-relative tw-bg-white"
+  >
+    <h3 class="tw-font-mono tw-text-lg tw-p-3">
+      {{
+        isTrigger
+          ? `${
+              step?.name && activeStepStage === "step_form"
+                ? step?.name
+                : "Trigger"
+            }`
+          : `${
+              step?.name && activeStepStage === "step_form"
+                ? step?.name
+                : "Step"
+            }`
+      }}
+    </h3>
+    <hr />
     <div class="tw-text-3xl tw-p-3">
       <!-- this is not rendered anywhere, just the state it generates is needed -->
       <Tabs
         :tab_list="fantomTabsForStepCreationStage"
         :value="activeStepStage"
-        :query_name="'step_stage_'+localId"
+        :query_name="'step_stage_' + localId"
         @model-value="($event:any) => activeStepStage = $event"
       />
-      <div v-if="activeStepStage==='step_selection'">
-        <TriggerList v-if="isTrigger" :actions="{ onSelect: handleStoreTriggerStep }" />
+      <div v-if="activeStepStage === 'step_selection'">
+        <TriggerList
+          v-if="isTrigger"
+          :actions="{ onSelect: handleStoreTriggerStep }"
+        />
         <div v-else>
           <FunctionList :actions="{ onSelect: handleStoreFunctionStep }" />
         </div>
       </div>
 
-      <div v-else-if="activeStepStage==='step_form'">
+      <div v-else-if="activeStepStage === 'step_form'">
         <button
           @click="onBack"
-          class="tw-text-sm tw-px-2 tw-py-1 tw-bg-gray-200 hover:tw-bg-gray-300
-          tw-rounded-md tw-flex tw-gap-2 tw-items-center">
+          class="tw-text-sm tw-px-2 tw-py-1 tw-bg-gray-200 hover:tw-bg-gray-300 tw-rounded-md tw-flex tw-gap-2 tw-items-center"
+        >
           <v-icon>mdi-arrow-left</v-icon>
           <span>back</span>
         </button>
@@ -62,19 +64,28 @@
     <div
       v-show="canAddStep || hasSteps"
       :class="[
-        { 'after:tw-absolute after:tw-top-full after:tw-w-px after:tw-bg-orange-500 after:tw-h-[72px]': !isLastStep },
+        {
+          'after:tw-absolute after:tw-top-full after:tw-w-px after:tw-bg-black/50 after:tw-h-[72px]':
+            !isLastStep,
+        },
       ]"
-      class="tw-absolute tw-inset-x-0 tw-bottom-0
-      tw-flex tw-justify-center">
+      class="tw-absolute tw-inset-x-0 tw-bottom-0 tw-flex tw-justify-center"
+    >
       <button
         @click="emits('add-step')"
         :class="[
           { '!tw-flex': isLastStep && canAddStep },
-          { 'before:tw-absolute before:tw-bottom-full before:tw-w-px before:tw-bg-orange-500 before:tw-h-[20px]': canAddStep || hasSteps },
+          {
+            'before:tw-absolute before:tw-bottom-full before:tw-w-px before:tw-bg-black/50 before:tw-h-[20px]':
+              canAddStep || hasSteps,
+          },
+          {
+            'after:tw-absolute after:tw-top-full after:tw-w-px after:tw-bg-black/50 after:tw-h-[20px]':
+              !isLastStep,
+          },
         ]"
-        class="tw-absolute tw-top-[20px] tw-bg-gray-200 hover:tw-bg-gray-300
-        tw-rounded-lg tw-transition-colors tw-duration-300 tw-p-1
-        tw-hidden tw-justify-center">
+        class="tw-absolute tw-top-[20px] tw-bg-gray-200 hover:tw-bg-gray-300 tw-rounded-lg tw-transition-colors tw-duration-300 tw-p-1 tw-hidden tw-justify-center"
+      >
         <v-icon>mdi-plus</v-icon>
       </button>
     </div>
@@ -82,11 +93,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import TriggerList from '@/components/molecules/TriggerList.vue';
-import FunctionList from '@/components/molecules/FunctionList.vue';
-import Tabs from '@/components/Tabs.vue';
-import ReturnsForm from '@/components/molecules/ReturnsForm.vue';
+import { computed, ref } from "vue";
+import TriggerList from "@/components/molecules/TriggerList.vue";
+import FunctionList from "@/components/molecules/FunctionList.vue";
+import Tabs from "@/components/Tabs.vue";
+import ReturnsForm from "@/components/molecules/ReturnsForm.vue";
 import type {
   Trigger,
   Function,
@@ -94,61 +105,68 @@ import type {
   WorkflowFunctionData,
   TriggerParameter,
   FunctionParameter,
-} from '@/types/workflow';
-import { useWorkflowStore } from '@/stores/workflow';
+} from "@/types/workflow";
+import { useWorkflowStore } from "@/stores/workflow";
 
 const props = defineProps<{
-  step: WorkflowTriggerData | WorkflowFunctionData | undefined
-  isTrigger: boolean
-  isLastStep: boolean
-}>()
+  step: WorkflowTriggerData | WorkflowFunctionData | undefined;
+  isTrigger: boolean;
+  isLastStep: boolean;
+}>();
+
+type StepOutput = { name: string; type: string };
+
 const emits = defineEmits<{
-  (e: 'add-step'): void
-}>()
+  (e: "add-step", value?: StepOutput[]): void;
+}>();
 
-const localId = ref(props?.step?.localId ?? new Date().toJSON())
-const canAddStep = computed(()=>!!props?.step?.canAddNextStep || props.isTrigger) // if it's a trigger, it can always add a step
-const hasSteps = ref(!props.isLastStep)
+const localId = ref(props?.step?.localId ?? new Date().toJSON());
+const canAddStep = computed(
+  () => !!props?.step?.canAddNextStep || props.isTrigger
+); // if it's a trigger, it can always add a step
+const hasSteps = ref(!props.isLastStep);
 
-const activeStepStage = ref()
+const activeStepStage = ref();
 const fantomTabsForStepCreationStage = [
   {
-    name: 'step selection',
-    slug: 'step_selection',
+    name: "step selection",
+    slug: "step_selection",
   },
   {
-    name: 'step form',
-    slug: 'step_form',
+    name: "step form",
+    slug: "step_form",
   },
   {
-    name: 'created',
-    slug: 'created',
+    name: "created",
+    slug: "created",
   },
-]
+];
 
-const workflowStore = useWorkflowStore()
+const workflowStore = useWorkflowStore();
 const handleStoreTriggerStep = (trigger: Trigger) => {
-  workflowStore.setSelectedTriggerStep({trigger, localId: localId.value})
-  activeStepStage.value = fantomTabsForStepCreationStage[1]
-}
+  workflowStore.setSelectedTriggerStep({ trigger, localId: localId.value });
+  activeStepStage.value = fantomTabsForStepCreationStage[1];
+};
 
 const handleStoreFunctionStep = (function_: Function) => {
-  workflowStore.setSelectedFunctionStep({function_, localId: localId.value})
-  activeStepStage.value = fantomTabsForStepCreationStage[1]
-}
+  workflowStore.setSelectedFunctionStep({ function_, localId: localId.value });
+  activeStepStage.value = fantomTabsForStepCreationStage[1];
+};
 
-const formToReturn = computed(()=>{
-  const stepInfo = props.isTrigger ? workflowStore.getTrigger : workflowStore.getStep(localId.value)
+const formToReturn = computed(() => {
+  const stepInfo = props.isTrigger
+    ? workflowStore.getTrigger
+    : workflowStore.getStep(localId.value);
   // if formData is available, it means that the step has been created
-  return stepInfo?.formData as (TriggerParameter | FunctionParameter)[] | null ??
-    workflowStore.getTriggerOrFunctionById({_id: stepInfo?._id ?? '', isTrigger: props.isTrigger})?.parameters
-})
+  return (
+    (stepInfo?.formData as (TriggerParameter | FunctionParameter)[] | null) ??
+    workflowStore.getTriggerOrFunctionById({
+      _id: stepInfo?._id ?? "",
+      isTrigger: props.isTrigger,
+    })?.parameters
+  );
+});
 const onBack = () => {
-  activeStepStage.value=fantomTabsForStepCreationStage[0]
-}
-
-const handleFormSubmit = (formData: any) => {
-  workflowStore.updateStep({localId: localId.value, data: formData, isTrigger: props.isTrigger})
-  activeStepStage.value=fantomTabsForStepCreationStage[2]
-}
+  activeStepStage.value = fantomTabsForStepCreationStage[0];
+};
 </script>
